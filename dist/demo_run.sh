05c294ec879656d5a21194f344a0d0b033a97764
Kat Martinez <kmartine@andrew.cmu.edu>
2017-04-24 00:59:50 +0000

    fixed dumb stepper bugs!!!!

--- dist/demo_run.sh
+++ dist/demo_run.sh
@@ -4,7 +4,7 @@ echo "Initializing Java control system."
 java -jar controlsys.jar -classpath shipbot.mission.Mission > javaout.txt &
 echo "Control system initialized."
 
-#./../../shipbot-hebi/armcontrol/armcontrol > hebiout.txt &
+./../../shipbot-hebi/armcontrol/armcontrol > hebiout.txt &
 
 echo "Establishing serial pipeline to Arduino."
 python SerialPipeline.py
